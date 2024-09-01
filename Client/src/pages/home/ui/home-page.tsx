cba55a63e003261208ecd6fbb22d516a0980ef85
Edanriell <Edanriell@gmail.com>
2024-09-01 14:02:11 +0300

    working on mobile home page

--- Client/src/pages/home/ui/home-page.tsx
+++ Client/src/pages/home/ui/home-page.tsx
@@ -2,6 +2,63 @@ import { FC } from "react";
 
 import { MainLayout } from "@widgets/layouts/main/ui";
 
+const products = [
+	{
+		image: "/images/raster/products/waffle-with-berries.jpg",
+		type: "Waffle",
+		name: "Waffle with Berries",
+		price: 6.5
+	},
+	{
+		image: "/images/raster/products/vanilla-bean-creme-brulee.jpg",
+		type: "Crème Brûlée",
+		name: "Vanilla Bean Crème Brûlée",
+		price: 7.0
+	},
+	{
+		image: "/images/raster/products/macaron-mix-of-five.jpg",
+		type: "Macaron",
+		name: "Macaron Mix of Five",
+		price: 8.0
+	},
+	{
+		image: "/images/raster/products/classic-tiramisu.jpg",
+		type: "Tiramisu",
+		name: "Classic Tiramisu",
+		price: 5.5
+	},
+	{
+		image: "/images/raster/products/pistachio-baklava.jpg",
+		type: "Baklava",
+		name: "Pistachio Baklava",
+		price: 4.0
+	},
+	{
+		image: "/images/raster/products/lemon-meringue-pie.jpg",
+		type: "Pie",
+		name: "Lemon Meringue Pie",
+		price: 5.0
+	},
+	{
+		image: "/images/raster/products/red-velvet-cake.jpg",
+		type: "Cake",
+		name: "Red Velvet Cake",
+		price: 4.5
+	},
+	{
+		image: "/images/raster/products/salted-caramel-brownie.jpg",
+		type: "Brownie",
+		name: "Salted Caramel Brownie",
+		price: 5.5
+	},
+	{
+		image: "/images/raster/products/vanilla-panna-cotta.jpg",
+		type: "Panna Cotta",
+		name: "Vanilla Panna Cotta",
+		price: 6.5
+	}
+];
+
 export const HomePage: FC = () => {
 	return (
 		<MainLayout>
