e4ca580fccde6d3602ab8973854273089810faa5
Edanriell <Edanriell@gmail.com>
2024-09-09 06:36:43 +0300

    finished button

--- Client/src/shared/ui/button/button.tsx
+++ Client/src/shared/ui/button/button.tsx
@@ -1,5 +1,15 @@
-import { FC } from "react";
+import "./button.less";
 
-export const Button: FC = () => {
-	return null;
+import { ComponentPropsWithoutRef, FC, ReactNode } from "react";
+
+type ButtonProps = {
+	children: ReactNode;
+} & ComponentPropsWithoutRef<"button">;
+
+export const Button: FC<ButtonProps> = ({ children, ...rest }) => {
+	return (
+		<button className="button" type="button" {...rest}>
+			{children}
+		</button>
+	);
 };
